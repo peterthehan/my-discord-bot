bcdb584d182bdae29843d41dade0e639071b159a
Peter Han <peter.han.9493@gmail.com>
2022-12-05 23:12:37 -0800

    Fix nitro edgecase with bad autocompleted strings

--- src/commands/nitro.ts
+++ src/commands/nitro.ts
@@ -14,9 +14,13 @@ function getAnimatedEmojis(
   interaction: BaseInteraction
 ): Collection<string, GuildEmoji> {
   return (
-    interaction.guild?.emojis.cache.filter(
-      (emoji) => emoji.animated && Boolean(emoji.name)
-    ) ?? new Collection()
+    interaction.guild?.emojis.cache
+      .filter((emoji) => emoji.animated && Boolean(emoji.name))
+      .reduce(
+        (emojis, emoji) =>
+          emojis.set(emoji.name?.toLowerCase() as string, emoji),
+        new Collection()
+      ) ?? new Collection()
   );
 }
 
@@ -24,7 +28,7 @@ const optionsRange = range(1, 3);
 
 const data = new SlashCommandBuilder()
   .setName("nitro")
-  .setDescription("Send animated emoji(s).")
+  .setDescription("Send animated emojis.")
   .setDMPermission(false);
 
 optionsRange.forEach((i) => {
@@ -43,14 +47,11 @@ optionsRange.forEach((i) => {
 const command: DiscordCommand = {
   data,
   async autocomplete(interaction: AutocompleteInteraction) {
-    const animatedEmojis = getAnimatedEmojis(interaction);
+    const animatedEmojiNames = [...getAnimatedEmojis(interaction).keys()];
     const focusedValue = interaction.options.getFocused().toLowerCase();
-    const filtered = animatedEmojis
-      .filter(
-        (emoji) => emoji.name && emoji.name.toLowerCase().includes(focusedValue)
-      )
-      .map((emoji) => emoji.name)
-      .slice(0, 25) as string[];
+    const filtered = animatedEmojiNames
+      .filter((emojiName) => emojiName.includes(focusedValue))
+      .slice(0, 25);
 
     await interaction.respond(
       filtered.map((choice) => ({ name: choice, value: choice }))
@@ -61,14 +62,15 @@ const command: DiscordCommand = {
     const emojis = optionsRange
       .map((i) => interaction.options.getString(`emoji-${i}`))
       .filter(Boolean) as string[];
-    const mapped = emojis.map(
-      (emoji) =>
-        animatedEmojis.find(
-          (animatedEmoji) => animatedEmoji.name === emoji
-        ) as GuildEmoji
-    );
+    const mapped = emojis
+      .filter((emoji) => animatedEmojis.has(emoji))
+      .map((emoji) => animatedEmojis.get(emoji)) as GuildEmoji[];
 
-    await interaction.reply(mapped.join(" "));
+    await interaction.reply(
+      mapped.length
+        ? mapped.join(" ")
+        : { content: "No animated emojis to send were found!", ephemeral: true }
+    );
   },
 };
 
